1c29155efb5a011acd86bcec89a7f05d0c00b3b2
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-10-06 11:02:05 +0100

    Delete other test command

--- src/commands/tools/ping.js
+++
@@ -1,8 +0,0 @@
-const { SlashCommandBuilder } = require("discord.js");
-
-module.exports = {
-  data: new SlashCommandBuilder().setName(`test`).setDescription(`test`),
-  async execute(interaction,client) {
-    console.log(client.guilds.fetch(`1051953804096049264`));
-  },
-};
